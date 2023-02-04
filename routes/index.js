4c2fe20e28f9a77fb5547fc752ad6ab3300e877e
Marta M <marmacan.upv@gmail.com>
2023-02-04 13:02:29 +0100

    Arreglada funcion para mandar emails

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const nodeMail = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,42 +17,48 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'contact' });
 });
 
-// MANDAR EMAIL:
-async function mainMail(email, message) {
-  const transporter = await nodeMail.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS,
-    },
-  });
-  const mailOption = {
-    from: email,
-    to: process.env.GMAIL_USER,
-    subject: "Pruebas Email",
-    text: message
-  };
-  try {
-    await transporter.sendMail(mailOption);
-    return Promise.resolve("Message Sent Successfully!");
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
 
-// POST para enviar email
-router.post("/contact", async (req, res, next) => {
+// // POST para enviar email
+router.post("/contact", (req, res, next) => {
   const user_email = req.body.email
   const user_message = req.body.message
   // console.log(user_email)
   // console.log(user_message)
-  try {
-    await mainMail(user_email, user_message);
-    res.send("Message Successfully Sent!");
-  } 
-  catch (error) {
-    res.send("No se ha podido mandar el mensaje");
-  }
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    }
+
+  });
+
+  var mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER,
+    subject: 'Email Formulario Contact',
+    text: `${user_email} ha mandado el siguiente mensaje: ${user_message}` 
+  };
+
+  transporter.sendMail(mailOptions,(err,res)=>{
+    if(err){
+      console.log(err);
+    }
+    else {
+      console.log('Mensaje enviado');
+    }
+  });
+
+  res.redirect('/send-email')
+
+});
+
+// EMAIL MANDADO PAGE
+router.get('/send-email', function(req, res, next) {
+  res.render('email_mandado', { title: 'Email mandado' });
 });
 
 module.exports = router;
